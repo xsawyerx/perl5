Leprevost/perl

XXX dist/IO/t/io_utf8argv.t: Temporarily skip if EBCDIC

dist/IO/t/io_utf8argv.t
@@ -6,6 +6,7 @@ BEGIN {
 	exit 0;
     }
     require($ENV{PERL_CORE} ? "../../t/test.pl" : "./t/test.pl");
+    skip_all("Needs EBCDIC porting fcn to convert utf8 bytes to utfebcid)") if $::EBCDIC;
 }
 
 use utf8;

xsawyerx/perl5

dist/IO/t/io_utf8argv.t
@@ -6,6 +6,7 @@ BEGIN {
 	exit 0;
     }
     require($ENV{PERL_CORE} ? "../../t/test.pl" : "./t/test.pl");
+    skip_all("Needs EBCDIC porting fcn to convert utf8 bytes to utfebcid)") if $::IS_EBCDIC;
 }
 
 use utf8;