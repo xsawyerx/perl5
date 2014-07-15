656a2fe6b52ecd7e9e706912661dfc8f656af97e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-07-15 10:23:35 +0100

    Update Test-Simple to CPAN version Test-Simple-1.301001_009
      *** testing only ***

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1042,7 +1042,7 @@ use File::Glob qw(:case);
     },
 
     'Test::Simple' => {
-        'DISTRIBUTION' => 'EXODIST/Test-Simple-1.301001_008.tar.gz',
+        'DISTRIBUTION' => 'EXODIST/Test-Simple-1.301001_009.tar.gz',
         'FILES'        => q[cpan/Test-Simple],
         'EXCLUDED'     => [
             qr{^t/xt},
